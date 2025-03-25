32797a43d4b5b47ba8542779233599b701628bfc
seimes <simon.wolfsteiner@a1.net>
2025-03-25 11:29:35 +0100

    Modified Main.java

--- src/main/java/at/technikum/w3resourceOOP/exercise3/Main.java
+++ src/main/java/at/technikum/w3resourceOOP/exercise3/Main.java
@@ -3,7 +3,7 @@ package at.technikum.w3resourceOOP.exercise3;
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(4.5, 6);
-
+        
         System.out.println("Rectangle with width: " + rectangle.getWidth() + " and height: " + rectangle.getHeight());
         System.out.println("Area: " + rectangle.getArea() + " square meters");
         System.out.println("Perimeter: " + rectangle.getPerimeter() + " meters");
