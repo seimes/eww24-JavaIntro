97b5ef966d6360dda0c0a2f8b42cea71a01ad971
seimes <simon.wolfsteiner@a1.net>
2025-03-25 11:23:15 +0100

    Added exercise 3

---
+++ src/main/java/at/technikum/w3resourceOOP/exercise3/Rectangle.java
@@ -0,0 +1,35 @@
+package at.technikum.w3resourceOOP.exercise3;
+
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getPerimeter() {
+        return 2 * width + 2 * height;
+    }
+}
