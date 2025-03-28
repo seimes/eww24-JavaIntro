89410210d0bd3be980af68436496beca56199793
seimes <simon.wolfsteiner@a1.net>
2025-03-28 15:57:14 +0100

    Refactored RoboterSteuerung

--- src/main/java/at/technikum/exercises/chapter4/RoboterSteuerung.java
+++ src/main/java/at/technikum/exercises/chapter4/RoboterSteuerung.java
@@ -3,41 +3,42 @@ package at.technikum.exercises.chapter4;
 import java.util.Scanner;
 
 public class RoboterSteuerung {
+    private static boolean checkValues(int min, int max, double value) {
+        if (value < min ||  value > max) {
+            System.out.println("** Fehler: Wertebereich");
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Art: ");
-        int art = 5;
+        int art;
 
         do {
+            System.out.print("Art: ");
             art = sc.nextInt();
+            double value;
             switch (art) {
                 case 1:
                     System.out.print(1 + ": ");
-                    double value = sc.nextDouble();
+                    value = sc.nextDouble();
                     System.out.printf("Abstand vorne: %.2f m\n", value);
-                    if (value < 0 || value > 10) {
-                        System.out.println("** Fehler: Wertebereich");
-                        return;
-                    }
+                    if(!checkValues(0, 10, value)) return;
                     break;
                 case 2:
                     System.out.print(1 + ": ");
                     value = sc.nextDouble();
                     System.out.printf("Abstand hinten: %.2f m\n", value);
-                    if (value < 0 || value > 10) {
-                        System.out.println("** Fehler: Wertebereich");
-                        return;
-                    }
+                    if(!checkValues(0, 10, value)) return;
                     break;
                 case 3:
                     String output = "Gyroskop: (";
                     for (int i = 1; i <= 3; i++) {
                         System.out.print(i + ": ");
                         value = sc.nextDouble();
-                        if (value < -250 || value > 250) {
-                            System.out.println("** Fehler: Wertebereich");
-                            return;
-                        }
+                        if(!checkValues(-250, 250, value)) return;
+
                         if (i != 3) {
                             output += String.format("%.2f, ", value);
                         } else {
@@ -51,10 +52,7 @@ public class RoboterSteuerung {
                     for (int i = 1; i <= 3; i++) {
                         System.out.print(i + ": ");
                         value = sc.nextDouble();
-                        if (value < 0 || value > 1) {
-                            System.out.println("** Fehler: Wertebereich");
-                            return;
-                        }
+                        if(!checkValues(0, 1, value)) return;
                         if (i != 3) {
                             output += String.format("%.2f, ", value);
                         } else {
